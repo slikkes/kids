71edbe1a05bcb954eab522162e8d4ce18a93cfae
slikkistvan@gmail.com <slikkistvan@gmail.com>
2018-07-06 23:01:27 +0200

    defined popup actions

--- app/src/main/java/com/example/slikk/asdf/ReadKidFragment.java
+++ app/src/main/java/com/example/slikk/asdf/ReadKidFragment.java
@@ -36,39 +36,54 @@ public class ReadKidFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_read_kid, container, false);
 
-        tvInfo = (ListView)view.findViewById(R.id.tv_info);
+        tvInfo = (ListView) view.findViewById(R.id.tv_info);
 
         List<Kid> kids = MainActivity.appDb.kidsDao().getKids();
 
         ArrayList<String> items = new ArrayList<>();
 
 
-        for(Kid kid : kids){
+        for (Kid kid : kids) {
             int id = kid.getId();
             String lname = kid.getLastname();
             String fname = kid.getFirstname();
-            items.add(lname +" "+ fname);
+            items.add(lname + " " + fname);
 //            info += "\n\nid: " + id + "\nlast name: " + lname + "\n first name: "+fname;
 
         }
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-          getActivity(),
-          android.R.layout.simple_list_item_1,
-          items
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                items
         );
 
         tvInfo.setAdapter(adapter);
         tvInfo.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, final int i, long l) {
-                PopupMenu popup = new PopupMenu(getActivity(), tvInfo);
+            public void onItemClick(AdapterView<?> adapterView, final View v, final int i, long l) {
+                PopupMenu popup = new PopupMenu(getActivity(), v);
                 popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
 
                 popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                     @Override
                     public boolean onMenuItemClick(MenuItem menuItem) {
-                        Toast.makeText(getActivity(),""+menuItem.getTitle()+" "+i,Toast.LENGTH_SHORT).show();
+
+                        switch (menuItem.getItemId()) {
+                            case R.id.popup_del:
+                                deletKid(i+1);
+                                break;
+                            case R.id.popup_addpoint:
+                                Toast.makeText(getActivity(), "soon add point", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.popup_update:
+                                Toast.makeText(getActivity(), "soon update", Toast.LENGTH_SHORT).show();
+                                break;
+                            default:
+                                Toast.makeText(getActivity(), "" + menuItem.getItemId(), Toast.LENGTH_SHORT).show();
+                                break;
+
+                        }
                         return true;
                     }
                 });
@@ -82,4 +97,14 @@ public class ReadKidFragment extends Fragment {
         return view;
     }
 
+
+    private void deletKid(int id) {
+
+        Kid kid = new Kid();
+        kid.setId(id);
+
+        MainActivity.appDb.kidsDao().deleteKid(kid);
+
+        Toast.makeText(getActivity(), "kid deleted", Toast.LENGTH_SHORT).show();
+    }
 }
